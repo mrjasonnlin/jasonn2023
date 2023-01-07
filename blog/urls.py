69b244b3dbf4d198a1764d8b6ae986deabf62c83
mrjas <mrjasonnlin@gmail.com>
2023-01-07 23:30:27 +0800

    hi

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from main import views
+# from main import views
 from django.conf.urls.static import static
 from django.conf import settings
 # from blog.settings import MEDIA_ROOT
