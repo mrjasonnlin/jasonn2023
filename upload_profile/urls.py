d49975c3a29e4664abe376e83b24562533cafb69
mrjas <mrjasonnlin@gmail.com>
2023-01-20 10:14:58 +0800

    hi

--- upload_profile/urls.py
+++ upload_profile/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 app_name = 'upload_profile'
 urlpatterns = [
-    path('', include(('upload_profile.urls', 'upload_profile'), namespace='upload_profile')),
+#    path('', include(('upload_profile.urls', 'upload_profile'), namespace='upload_profile')),
+    path('upload_profile/', views.upload_profile, name='upload_profile'),
 #    path('', views.upload_profile, name='upload_profile'),
-#    path('add/', views.add, name='add'),
-#    path('detail/', views.detail, name='detail'),
+    path('add/', views.add, name='add'),
+    path('detail/', views.detail, name='detail'),
 
 ]
