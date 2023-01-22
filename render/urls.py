62a0c3e0e9efd461de28f0c0cafcddf041c533ad
mrjas <mrjasonnlin@gmail.com>
2023-01-22 22:22:02 +0800

    hi

--- render/urls.py
+++ render/urls.py
@@ -1,16 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include(('main.urls', 'main'), namespace='main')),
-#    path('', include(('account.urls', 'account'), namespace='account'))
-#    path('', include(('register.urls', 'register'), namespace='register')),
-#    path('', include(('login.urls', 'login'), namespace='login')),
-#    path('', include(('logout.urls', 'logout'), namespace='logout'))
-#    path('register/', views.register, name='register'),
-#    path('login/', views.login, name='login'),
-#    path('logout/', views.register, name='logout'),
 
 ]
--- render/views.py
+++ render/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.contrib import messages
-from django.urls.base import reverse
 
 
 # Create your views here.
 def index(request):
-    context = {'like': 'Django 很棒'}
     return render(request, 'render/index.html', {})
+
 
 def bike(request):
     """
@@ -14,11 +12,10 @@ def bike(request):
     """
     return render(request, 'render/bike.html')
 
+
 def main(request):
     """
     Render the main page
     """
     context = {'like': 'Django 很棒'}
     return render(request, 'main/main.html', context)
-
-
