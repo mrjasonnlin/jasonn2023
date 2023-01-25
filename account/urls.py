3a142c1ed6d8500ddd2f579885852547ca3779f4
mrjas <mrjasonnlin@gmail.com>
2023-01-25 10:33:10 +0800

    hi

--- account/urls.py
+++ account/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-#    path('register/', views.register, name='register'),
-#    path('login/', views.login, name='login'),
-#    path('logout/', views.logout, name='logout'),
+
 
 ]
