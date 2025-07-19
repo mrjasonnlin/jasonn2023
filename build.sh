171ea72814b38996bcbbafea1e49a704afc95de3
mrjasonnlin <mrjasonnlin@gmail.com>
2025-07-19 09:49:31 +0800

    p1

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+#!python manage.py makemigrations
 python manage.py migrate
 
